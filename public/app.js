d9f14caedff3038a8fbc3dbd8a2e774c2ee21e86
aptakqmf12 <leexodhks12@naver.com>
2021-10-23 16:56:29 +0900

    [기능최종완성] 삭제기능 완성 - firestore연동 완료.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,19 @@
 import {addItem} from './todo.js';
 'use strict';
 
+// form 기본기능 막기
+let todoForm = document.querySelector('.todoForm');
+todoForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+})
+// input 엔터시
+let inputTxt = document.querySelector('.item');
+inputTxt.addEventListener('keyup',(e) => {
+    if(e.keyCode === 13){
+        addItem();
+    }
+})
+// button 클릭시
 let addBtn = document.querySelector('.input__btn');
 addBtn.addEventListener("click", addItem);
-
 
--- public/todo.js
+++ public/todo.js
@@ -1,6 +1,5 @@
 let todoList = [];
 
-//데이터 받는부분
 const firebaseConfig = {
     apiKey: "AIzaSyAwnltMvwCLc6QBjEGX5hZcfkwfCJnmdlw",
     authDomain: "only-js-todolist.firebaseapp.com",
@@ -9,25 +8,27 @@ const firebaseConfig = {
     messagingSenderId: "1011094966726",
     appId: "1:1011094966726:web:28eee66d1fc0d03cf5a2c7",
     measurementId: "G-D6B64GCQWH"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
-
-  const db = firebase.firestore();
-  db.collection('todolist').get().then( (res) => {
+firebase.initializeApp(firebaseConfig);
+//화면초기에 데이터 받기
+const db = firebase.firestore();
+db.collection('todolist').get().then( (res) => {
     res.forEach( (doc) => {
-      todoList = [...todoList, doc.data()];
-      showItem();   
+        todoList = [...todoList, {todo : doc.data().todo, idx : doc.id }];
+        showItem();   
     });
-  });
+});
 
 let item = document.querySelector('.item');
 let showBox = document.querySelector('.show__box');
-
+// 추가버튼 눌릴시
 function addItem(){
     if(item.value != ""){
-        todoList = [...todoList, {todo : item.value, idx: generateRandomNum() }];
-        db.collection('todolist').add({todo: item.value, idx : generateRandomNum()})
+        //todoList에 데이터 추가
+        todoList = [...todoList, {todo : item.value}];
+        //db에 데이터 업로드
+        db.collection('todolist').add( {todo:item.value} );
     }
     else{
         alert("값을 입력해주세요")
@@ -35,25 +36,24 @@ function addItem(){
     item.value = "";
     item.focus();
     showItem();
-    console.log(todoList)
 }
 function showItem(){
-    
     // 뿌려진 리스트 리셋
     let prevTodo = document.querySelectorAll('.show__box > li');
     prevTodo.forEach(e => {
         e.remove();
     })
     // 리스트 다시 그림
+    console.log(todoList)
     todoList.forEach( e => {
         let todo = document.createElement('li');
         let todoDelete = document.createElement('button');
         todoDelete.className = "todoDelete";
         let todoTxt = document.createTextNode(e.todo);
+        todo.dataset.idx = e.idx;
 
         todoDelete.textContent = "delete";
         todo.appendChild(todoTxt);
-        todo.dataset.idx = e.idx;
 
         todo.append(todoDelete);
         showBox.append(todo);
@@ -61,30 +61,15 @@ function showItem(){
     deleteItem();
 }
 function deleteItem(){
-
     let todoDelete = document.querySelectorAll('.todoDelete');
     todoDelete.forEach( btn => {
         btn.addEventListener("click", e =>{
             let deletedList = e.target.closest('li');
-            todoList =  todoList.filter( todoList=> {return todoList.idx !== deletedList.dataset.idx});
-            
-                
+            todoList = todoList.filter( todos => {return todos.idx != deletedList.dataset.idx});
+            db.collection('todolist').doc(deletedList.dataset.idx).delete()
             showItem();
         });
-    })
-    
+    })   
 }
-function generateRandomNum(){
-    let str = "";
-    for(let i=0; i<6; i++){
-       str += Math.floor(Math.random() * 10);
-    }
-    return str;  
-}
-db.collection('todolist').doc().delete().then(() => {
-    console.log("Document successfully deleted!");
-}).catch((error) => {
-    console.error("Error removing document: ", error);
-});
 
 export  {addItem} ;
